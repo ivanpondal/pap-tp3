abab826ded23b5e7d2435860697b5d6575a86298
Martin Jedwabny <martiniedva@gmail.com>
2016-11-02 02:01:53 -0300

    Exercise 3

--- src/ej3/ej3.h
+++ src/ej3/ej3.h
@@ -3,11 +3,31 @@
 
 #include "../main.h"
 
+using namespace std;
+
 /*
 **  Exercise 3
 */
 
-// Here go declarations of functions which are exported by the problem solver,
-// global variables, etc.
+class Query {
+public:
+    // Constructors
+    Query() {}
+    ~Query() {}
+
+    // Properties
+    int c1, a1, c2, a2;
+
+    // Methods
+    friend istream& operator >>(istream& i, Query& query) {
+        i >> query.c1; query.c1 --;
+        i >> query.a1; query.a1 --;
+        i >> query.c2; query.c2 --;
+        i >> query.a2; query.a2 --;
+        return i;
+    }
+};
+
+void calculate_salary_difference(int C, int A, int Q, vector< vector<int> >& M, vector<Query>& queries, vector<int>& solutions);
 
 #endif // PAP_TP3_EJ3_H
