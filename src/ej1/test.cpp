5854c155009ac14817d52f8bfb7160a4f6b3fddc
Shai <shai.bianchi@mercadolibre.com>
2016-11-04 15:55:16 -0300

    [Exercise 1 - src] Tests para entregar sin caso borde que rompen el approach de mayores bordes propios

--- src/ej1/test.cpp
+++ src/ej1/test.cpp
@@ -7,41 +7,21 @@
 */
 
 void run_unit_tests() {
-	vector<pbl_case> pbl_cases = {
-		{"abcabdab", {-1,0,0,0,1,2,0,1,2}},
-		{"abcaby", {-1,0,0,0,1,2,0}},
-		{"abcdabd", {-1,0,0,0,0,1,2,0}},
-		{"participate in parachute", {-1,0,0,0,0,0,0,0,1,2,0,0,0,0,0,0,1,2,3,0,0,0,0,0,0}},
-		{"aaaaa", {-1,0,1,2,3,4}},
-		{"abcabax", {-1,0,0,0,1,2,1,0}},
-		{"abacabadabacabac", {-1,0,0,1,0,1,2,3,0,1,2,3,4,5,6,7,4}},
-		{"abcdabcd", {-1,0,0,0,0,1,2,3,4}},
-		{"potatopotato", {-1,0,0,0,0,0,0,1,2,3,4,5,6}}
+	vector<substr_case> substr_cases = { // tests del algoritmo completo
+		{"tato", "potatous", true}, // substring enteramente contenido
+		{"pota", "potato", true}, // prefijo
+		{"tato", "potato", true}, // sufijo
+		{"potato", "potato", true}, // palabra entera
+		{"a", "abc", true}, // patrón de tamaño 1
+		{"b", "abc", true}, // patrón de tamaño 1
+		{"c", "abc", true}, // patrón de tamaño 1
+		{"bla", "abl", false}, // negativo
+		{"bla", "ablba", false}, // negativo
+		{"holmund", "holamundo", false}, // negativo
+		{"abac", "abadabacabac", true}, // TS tiene prefijos con sucesión de bordes no trivial
+		{"aba", "babc", false}, // El prefijo "ababa" de TS = abababc tiene un borde de tamaño |T| = 3 que es cruzado, el string "aba"
 	};
 
-	vector<substr_case> substr_cases = {
-		{"TOTE", "ARISTOTELES", true},
-		{"Tote", "Aristoteles", false},
-		{"ritmo" ,"Algoritmo", true},
-		{"holmund", "holamundo", false},
-		{"tato", "potato", true},
-		{"potato", "potato", true},
-		{"aba", "babc", false}, // caso en el que aba+babc tiene el prefijo ababa que tiene un borde igual que |aba| pero es cruzado
-	};
-
-	cout << "prefix border lengths tests:" << endl;
-	for (size_t i = 0; i < pbl_cases.size(); ++i) {
-		auto c = pbl_cases[i];
-		vector<int> T = prefix_border_lengths(c.t);
-		if (T == c.expected) {
-			cout << "case " << i << ": ok" << endl;
-		} else {
-			cout << "case " << i << ": fail" << endl;
-		}
-	}
-
-	cout << endl;
-	cout << "substring tests:" << endl;
 	for (size_t i = 0; i < substr_cases.size(); ++i) {
 		auto c = substr_cases[i];
 		if (substring(c.t, c.s) == c.expected) {
